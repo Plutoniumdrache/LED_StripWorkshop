b1329a579968f6c63859e93089065a2919273d90
Plutoniumdrache <76872934+Plutoniumdrache@users.noreply.github.com>
2024-06-10 15:46:18 +0200

    Update LED_StripeWorkshop.ino

--- LED_StripeWorkshop.ino
+++ LED_StripeWorkshop.ino
@@ -4,7 +4,8 @@
 #include <avr/power.h>
 #endif
 
-// #define row
+// Hier werden die unterschiedlichen Beispiele ein und ausgeschaltet.
+// #define row 
 // #define SINGLECOLOR
 #define random
 
@@ -19,11 +20,11 @@ int pause=1000; // 100 Millisekunden Pause bis zur Ansteuerung der nächsten LED
 
 void setup() 
 {
-  randomSeed(analogRead(0));
+  randomSeed(analogRead(0)); // Startwert für die Zufallszahlgeneration
   pixels.begin(); // Initialisierung der NeoPixel
   #ifdef SINGLECOLOR
   for (i=0;i<=NUMPIXELS;i++){
-        pixels.setPixelColor(i, pixels.Color(75,150,0)); // Pixel leuchtet in der Farbe rot
+        pixels.setPixelColor(i, pixels.Color(100,0,0)); // Pixel leuchtet in der Farbe rot
   }
     pixels.show(); // Durchführen der Pixel-Ansteuerung
   #endif
@@ -32,7 +33,7 @@ void setup()
 void loop() 
 {
   #ifdef row
-  pixels.setPixelColor(i, pixels.Color(75,150,0)); // Pixel leuchtet in der Farbe Grün
+  pixels.setPixelColor(i, pixels.Color(0,100,0)); // Pixel leuchtet in der Farbe Grün
   pixels.setPixelColor(i-1, pixels.Color(0,0,0)); // Der vorherige Pixel wird abgeschaltet
   pixels.show(); // Durchführen der Pixel-Ansteuerung
     
@@ -41,7 +42,7 @@ void loop()
   }
   pixels.show(); // Durchführen der Pixel-Ansteuerung
   delay (pause);
-  i=i+1; // Die Variable "i" wird um eine Zahl vergrößert. Die neue Zahl "i" ist dann die nächste LED im Led-Ring
+  i = i + 1; // Die Variable "i" wird um eine Zahl vergrößert. Die neue Zahl "i" ist dann die nächste LED im Led-Ring
   if (i==NUMPIXELS) {
     i=0; // Wenn die Variable den Wert 9 erreicht hat, wird die Variable auf den Wert "0" zurück gesetzt, da die Nummerierung der LEDs nur von 0 bis 8 geht.
   }
@@ -50,14 +51,14 @@ void loop()
   #ifdef random
     for (int i = 0; i <= NUMPIXELS; i++){
       { int brightness = 50;
-        int r = random(brightness);
-        int g = random(brightness);
-        int b = random(brightness);
-        pixels.setPixelColor(i, pixels.Color(r,g,b)); // Pixel leuchtet in der Farbe Grün      
+        int r = random(brightness); // Zufällige Helligkeit der Farbe rot
+        int g = random(brightness); // Zufällige Helligkeit der Farbe grün
+        int b = random(brightness); // Zufällige Helligkeit der Farbe blau
+        pixels.setPixelColor(i, pixels.Color(r,g,b)); // Pixel leuchtet in einer bunten Farbe      
       }
     }
     pixels.show(); // Durchführen der Pixel-Ansteuerung
-    delay (pause);
+    delay (pause); 
   #endif
 
 }
